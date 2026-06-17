b6c074aa1818c7081847c5bbff4a8ea00798c581
Julien Moutoussamy <jmoutous@student.42lyon.fr>
2026-06-17 15:27:29 +0200

    ADD: find_separator() to get_data()

--- utils.py
+++ utils.py
@@ -1,9 +1,21 @@
+import csv
 import pandas as pd
 from pathlib import Path
 
 
+def find_separator(file: Path) -> str:
+    """Find the separator used in a CSV file."""
+    sniffer = csv.Sniffer()
+    with open(file) as f:
+        separator: str = sniffer.sniff(f.read(500)).delimiter
+
+    return separator
+
+
 def get_data(path: Path) -> pd.DataFrame:
-    data: pd.DataFrame = pd.read_csv(path, sep=",")
+    """Read a CSV file and return a pandas DataFrame."""
+    separator: str = find_separator(path)
+    data: pd.DataFrame = pd.read_csv(path, sep=separator)
     if "Index" in data.columns:
         data = data.set_index("Index")
     return data
