90ed0125e366bc063c8226c91b988d4ec5efcef2
Sarah Ponthus <sponthus@student.42lyon.fr>
2026-07-01 11:29:09 +0200

    ADD: describe.py, added Trimean + moved Percentiles in utils

--- describe.py
+++ describe.py
@@ -1,46 +1,8 @@
 import sys
-import numpy as np
 import pandas as pd
-import typing as tp
 from parsing import parse_describe_args
 import argparse
-from utils import ft_count, ft_mean, ft_deviation
-
-
-class Percentiles(tp.NamedTuple):
-    """A named tuple to hold the percentiles of a dataset"""
-    min: float
-    quartile_25: float
-    quartile_50: float
-    quartile_75: float
-    max: float
-
-
-def ft_percentiles(data: pd.Series, count: int) -> Percentiles:
-    """Calculate the percentiles of a pandas Series"""
-    assert count != 0, "count can not be null"
-    sorted_data: np.ndarray = np.sort(data)
-
-    min: float = sorted_data[0]
-    max: float = sorted_data[-1]
-
-    if count % 2 == 0:
-        quartile_25 = float(sorted_data[round(count * 1 / 4) - 1])
-        quartile_50 = float(sorted_data[round(count * 1 / 2) - 1])
-        quartile_75 = float(sorted_data[round(count * 3 / 4) - 1])
-    else:
-        quartile_25 = float(sorted_data[round((count + 1) * 1 / 4) - 1])
-        quartile_50 = float(sorted_data[round((count + 1) * 1 / 4) - 1])
-        quartile_75 = float(sorted_data[round((count + 1) * 1 / 4) - 1])
-
-    percentiles = Percentiles(
-        min=min,
-        quartile_25=quartile_25,
-        quartile_50=quartile_50,
-        quartile_75=quartile_75,
-        max=max
-    )
-    return percentiles
+from utils import Percentiles, ft_percentiles, ft_count, ft_mean, ft_deviation, ft_trimean
 
 
 def describe(data: pd.DataFrame):
@@ -55,11 +17,13 @@ def describe(data: pd.DataFrame):
             continue
         mean = ft_mean(col_data, count)
         percentiles: Percentiles = ft_percentiles(col_data, count)
+        trimean: float = ft_trimean(percentiles)
         variance, std = ft_deviation(col_data, mean, count)
         statistics.append({
             "Name": col,
             "Count": count,
             "Mean": mean,
+            "Trimean": trimean,
             "Std": std,
             "Variance": variance,
             "Min": percentiles.min,
--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import csv
 import pandas as pd
+import typing as tp
+import numpy as np
 from pathlib import Path
-
 
 COLORS_HOUSES = {
     "Slytherin": (0, 1, 0),
@@ -9,6 +10,42 @@ COLORS_HOUSES = {
     "Ravenclaw": (0, 0, 1),
     "Hufflepuff": (1, 1, 0)
 }
+
+
+class Percentiles(tp.NamedTuple):
+    """A named tuple to hold the percentiles of a dataset"""
+    min: float
+    quartile_25: float
+    quartile_50: float
+    quartile_75: float
+    max: float
+
+
+def ft_percentiles(data: pd.Series, count: int) -> Percentiles:
+    """Calculate the percentiles of a pandas Series"""
+    assert count != 0, "count can not be null"
+    sorted_data: np.ndarray = np.sort(data)
+
+    min: float = sorted_data[0]
+    max: float = sorted_data[-1]
+
+    if count % 2 == 0:
+        quartile_25 = float(sorted_data[round(count * 1 / 4) - 1])
+        quartile_50 = float(sorted_data[round(count * 1 / 2) - 1])
+        quartile_75 = float(sorted_data[round(count * 3 / 4) - 1])
+    else:
+        quartile_25 = float(sorted_data[round((count + 1) * 1 / 4) - 1])
+        quartile_50 = float(sorted_data[round((count + 1) * 1 / 4) - 1])
+        quartile_75 = float(sorted_data[round((count + 1) * 1 / 4) - 1])
+
+    percentiles = Percentiles(
+        min=min,
+        quartile_25=quartile_25,
+        quartile_50=quartile_50,
+        quartile_75=quartile_75,
+        max=max
+    )
+    return percentiles
 
 
 def ft_count(data: pd.Series) -> int:
@@ -19,11 +56,17 @@ def ft_count(data: pd.Series) -> int:
     return count
 
 
+def ft_trimean(percentiles: Percentiles) -> float:
+    """Trimean or Tukey's trimean, weighted average of distribution quartiles"""
+    trimean: float = (percentiles.quartile_25 + 2 * percentiles.quartile_50 + percentiles.quartile_75) / 4
+    return trimean
+
+
 def ft_mean(data: pd.Series, count: int) -> float:
     """Calculate the mean of a pandas Series"""
     assert count != 0, "count can not be null"
     mean: float = float("NaN")
-    total: int = 0
+    total: float = float(0)
 
     for item in data:
         total += item
