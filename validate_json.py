a1394b15a6d16e83aa0e6d9ec1eb806ef302f88f
Julien Moutoussamy <jmoutous@student.42lyon.fr>
2026-07-03 15:42:16 +0200

    ADD: validate_json.py: json valdation

---
+++ validate_json.py
@@ -0,0 +1,92 @@
+import json
+import jsonschema
+from jsonschema import validate
+from pathlib import Path
+
+MODEL_SCHEMA = {
+  "type": "object",
+  "properties": {
+    "class_enum": {
+      "type": "object",
+      "properties": {
+        "Ravenclaw": {
+          "type": "integer"
+        },
+        "Slytherin": {
+          "type": "integer"
+        },
+        "Gryffindor": {
+          "type": "integer"
+        },
+        "Hufflepuff": {
+          "type": "integer"
+        }
+      },
+      "required": [
+        "Ravenclaw",
+        "Slytherin",
+        "Gryffindor",
+        "Hufflepuff"
+      ]
+    },
+    "nb_classes": {
+      "type": "integer"
+    },
+    "nb_features": {
+      "type": "integer"
+    },
+    "class_col": {
+      "type": "string"
+    },
+    "features_cols": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      }
+    },
+    "weights": {
+      "type": "array",
+      "items": {
+        "type": "array",
+        "items": {
+          "type": "number"
+        }
+      }
+    },
+    "biases": {
+      "type": "array",
+      "items": {
+        "type": "array",
+        "items": {
+          "type": "number"
+        }
+      }
+    }
+  },
+  "required": [
+    "class_enum",
+    "nb_classes",
+    "nb_features",
+    "class_col",
+    "features_cols",
+    "weights",
+    "biases"
+  ],
+  "$schema": "https://json-schema.org/draft/2020-12/schema"
+}
+
+def validate_json(file: Path) -> None:
+    try:
+        with open(file) as f:
+            my_json = json.load(f)
+
+        validate(instance=my_json, schema=MODEL_SCHEMA)
+
+    except jsonschema.exceptions.ValidationError as ve:
+        print(f"Validation error: {ve.message}")
+
+    except jsonschema.exceptions.SchemaError as se:
+        print(f"Schema error: {se.message}")
+
+    except Exception as e:
+        print(f"Error: {e}")
