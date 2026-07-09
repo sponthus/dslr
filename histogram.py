c97570dc6ed3337326ba0b6ac833e71d44a78bbd
Julien Moutoussamy <jmoutous@student.42lyon.fr>
2026-07-09 16:55:28 +0200

    MOD: subplot with better legend

--- histogram.py
+++ histogram.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+from matplotlib.patches import Patch
 import pandas as pd
 import matplotlib.pyplot as plt
 from utils.utils import COLORS_HOUSES
@@ -10,9 +11,14 @@ def histogram(data: pd.DataFrame) -> None:
     """Plot histograms for each numeric column in the dataset,
     grouped by Hogwarts House."""
     numeric_col = [col for col in data.columns if data[col].dtype == float]
+    nb_col = len(numeric_col)
+    graph_per_row = 5
+    nb_row = nb_col // graph_per_row + (1 if nb_col % graph_per_row else 0)
+    fig = plt.figure(figsize=(6 * graph_per_row, 5 * nb_row), num="Histogram")
+    fig.suptitle("Histograms by Hogwarts House for each feature", size=25, y=0.98)
 
-    for col in numeric_col:
-        plt.figure(num="Histogram")
+    for i, col in enumerate(numeric_col):
+        plt.subplot(nb_row, graph_per_row, i + 1)
         for house, color in COLORS_HOUSES.items():
             color_w_transparency = (*color, 0.5)
             plt.hist(
@@ -20,12 +26,20 @@ def histogram(data: pd.DataFrame) -> None:
                 fc=color_w_transparency,
                 label=house
             )
-            plt.xlabel("Mark")
-            plt.ylabel("Number of students")
-            plt.legend()
-        plt.title(col)
-        plt.show()
+            plt.xlabel("Mark", fontsize=16)
+            plt.ylabel("Number of students", fontsize=16)
+        plt.title(col, fontsize=20)
 
+    fig.legend(
+        handles=[Patch(color=color, label=house) for house, color in COLORS_HOUSES.items()],
+        loc="upper center",
+        bbox_to_anchor=(0.5, 0.96),
+        ncol=len(COLORS_HOUSES),
+        frameon=False,
+        fontsize=12
+    )
+    plt.tight_layout(rect=(0, 0, 1, 0.96))
+    plt.show()
 
 def main():
     try:
